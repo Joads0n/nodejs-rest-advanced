c86775fbc897e5add12c7c99ed1683ab03a412ea
Joadson Silva <joadson83@hotmail.com>
2021-12-27 23:14:24 -0300

    Validação do id do Fornecedor

--- api/rotas/fornecedores/produtos/TabelaProdutos.js
+++ api/rotas/fornecedores/produtos/TabelaProdutos.js
@@ -1,5 +1,5 @@
 const Model = require('./ModeloTabelaProduto')
-
+// DAO
 module.exports = {
     listar (idFornecedor) {
         return Model.findAll({
@@ -7,5 +7,31 @@ module.exports = {
                 fornecedor: idFornecedor
             }
         })
+    },
+    inserir (dados){
+        return Model.create(dados)
+    },
+    remover (idProduto, idFornecedor){
+        return Model.destroy({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
+            }
+        })
+    },
+    async pegarPorId (idProduto, idFornecedor) {
+        const encontrado = await Model.findOne({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
+            },
+            raw: true
+        })
+
+        if (!encontrado) {
+            throw new Error('Produto não foi encontrado!')
+        }
+
+        return encontrado
     }
 }
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,9 +1,55 @@
 const roteador = require('express').Router({ mergeParams: true }) /* Indica para acessar os parametros do roteador acima */
 const Tabela = require('./TabelaProdutos')
+const Produto = require('./Produto')
 
 roteador.get('/', async (req, res) => {
     const produtos = await Tabela.listar(req.params.idFornecedor)
     res.send(JSON.stringify(produtos))
 })
+
+roteador.post('/', async (req, res, proximo) => {
+    try {
+        const idFornecedor = req.params.idFornecedor
+        const corpo = req.body
+        const dados = Object.assign({}, corpo, {fornecedor: idFornecedor})
+        const produto = new Produto(dados)
+        await produto.criar()
+        res.status(201)
+        res.send(produto)
+    } catch (erro) {
+        proximo(erro)
+    }
+})
+
+roteador.delete('/:id', async (req, res) => {
+    const dados = {
+        id: req.params.id,
+        fornecedor: req.params.idFornecedor
+    }
+    const produto = new Produto(dados)
+    await produto.apagar()
+    res.status(204)
+    res.end()
+})
+
+//Função não está retornando o produto de id especifico
+roteador.get('/:id', async (req, res, proximo) => {
+    try {
+        const dados = {
+            id: req.params.id,
+            fornecedor: req.fornecedor.id
+        }
+        //console.log(dados.id)
+    
+        const produto = new Produto(dados)
+        await produto.carregar()
+        res.send(JSON.stringify(produto))
+    } catch (erro) {
+        proximo(erro)
+    }
+
+})
+
+
 
 module.exports = roteador
